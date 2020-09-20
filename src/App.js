c73f0f3f7981d5a07e5d6484c469ccddb7c4fd4a
nede_dan <nede.dan@gmail.com>
2020-09-20 15:59:46 +0200

    display basic markers

--- src/App.js
+++ src/App.js
@@ -1,37 +1,78 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { Map, Marker, Popup, TileLayer } from "react-leaflet";
 import { loadData, saveData } from './components/GS';
 import './App.css';
 
-function App() {
-  const entry = {
-    active: 0,
-    id: 'UUID',
-    title: 'Title here',
-    img: 'img source',
-    coordinates: '[lat, long]',
-    description: 'Lorem Ipsum'
+//https://blog.logrocket.com/how-to-use-react-leaflet/
+
+class App extends Component {
+  state = {
+    markers: []
   }
+
+  async getMarkerData() {
+    const rows = await loadData();
+    const markers = rows.filter(row => parseInt(row.active)).map(row => {
+      const { active, id, title, img, lat, long, description } = row;
+      return {
+        active,
+        id,
+        title,
+        img,
+        coordinates: {
+          lat,
+          long
+        },
+        description
+      }
+    })
+
+    this.setState({
+      markers: markers
+    })
+  }
+  componentDidMount() {
+    this.getMarkerData();
+  }
+
+  // const entry = {
+  //   active: 0,
+  //   id: 'UUID',
+  //   title: 'Title here',
+  //   img: 'img source',
+  //   coordinates: {
+  //     lat: 234,
+  //     long: 45678
+  //   },
+  //   description: 'Lorem Ipsum'
+  // }
 
   //saveData(entry)
 
-  const addMarkers = async () => {
-    const rows = await loadData();
-    const activemarkers = rows.filter(row => parseInt(row.active))
+  render() {
+    const { markers } = this.state;
+    return (
+      <Map center={[45.270340, 25.050310]} zoom={13}>
+        {markers.map(marker => (
+          <Marker
+            key={marker.id}
+            position={[
+              marker.coordinates.lat,
+              marker.coordinates.long
+            ]}
+            // onClick={() => {
+            //   setActivePark(park);
+            // }}
+          />
+        ))}
 
-    console.log(activemarkers)
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        />
+      </Map>
+    );
   }
-
-  addMarkers();
-
-  return (
-    <Map center={[45.270340, 25.050310]} zoom={13}>
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-      />
-    </Map>
-  );
 }
 
 export default App;
